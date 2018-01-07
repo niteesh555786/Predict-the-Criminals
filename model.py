5ab8396ef08425f335221825f29a8768e1485296
niteesh555786 <niteesh555786@gmail.com>
2018-01-07 13:28:32 -0800

    files added

---
+++ model.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan  3 10:52:42 2018
+
+@author: Jaadugar
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+df=pd.read_csv('criminal_train.csv')
